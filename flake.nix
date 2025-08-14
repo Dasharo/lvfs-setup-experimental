8d8bb1061dcc86ab7376c50200f6fe762a449aed
Artur Kowalski <arturkow2000@gmail.com>
2025-08-14 18:15:53 +0200

    WIP attempt to run LVFS server with Docker + Nix
    Signed-off-by: Artur Kowalski <arturkow2000@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "A flake for rust project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    dream2nix.url = "github:nix-community/dream2nix";
+    nixpkgs.follows = "dream2nix/nixpkgs";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-parts,
+      dream2nix,
+      nix2container,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        ./nix/lvfs_container.nix
+      ];
+
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        { pkgs, inputs', ... }:
+        let
+          llvm = pkgs.llvmPackages_latest;
+          stdenv = pkgs.stdenvAdapters.overrideCC llvm.stdenv llvm.clangUseLLVM;
+          mkShell = pkgs.mkShell.override { inherit stdenv; };
+          package = dream2nix.lib.evalModules {
+            packageSets.nixpkgs = pkgs;
+            modules = [
+              ./nix/lvfs.nix
+              {
+                paths.projectRoot = ./.;
+                paths.projectRootFile = "flake.nix";
+                paths.package = ./.;
+              }
+            ];
+          };
+        in
+        {
+          _module.args.pkgs = inputs'.nixpkgs.legacyPackages;
+
+          devShells.default = mkShell {
+            # Include dependencies for LVFS
+            inputsFrom = [ package.devShell ];
+            # And LVFS itself
+            packages = [ package ];
+            env.LD_LIBRARY_PATH = "${pkgs.gnutls.out}/lib";
+          };
+          packages.default = package;
+        };
+    };
+}
